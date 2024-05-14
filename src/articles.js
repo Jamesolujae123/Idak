05888dc79274a31d7dbecb0c0211cc6b48443203
James olujae <96151013+Jamesolujae123@users.noreply.github.com>
2024-05-14 06:16:28 -0700

    Update articles.js

--- src/articles.js
+++ src/articles.js
@@ -12,29 +12,29 @@ return (<>
 
 <div className='featured-a'>
     <div className='left-a'><img className="art1" src={art1} />
-    <p>February 4, 2023</p>
-    <h4>Plan the Perfect Vacation</h4>
-    <p>Discover expert advice on how to find the ideal rental property that meets your needs and budget.</p></div>
+    <p className="date">February 4, 2023</p>
+    <h4 className="topic">Plan the Perfect Vacation</h4>
+    <p className="note">Discover expert advice on how to find the ideal rental property that meets your needs and budget.</p></div>
 
    
 <div className='right-a'> <div className='mid-art'><img className='art2' src={art2} art="art2" /> 
-<div className='mid-art-abt'><p>May 7, 2023</p>
-<h4>Car Rental hacks for smooth Journey</h4>
-<p>Learn insider tips for getting the best deals and making the most of your rental car experience.</p></div>
+<div className='mid-art-abt'><p className="date">May 7, 2023</p>
+<h4 className="topic">Car Rental hacks for smooth Journey</h4>
+<p className="note">Learn insider tips for getting the best deals and making the most of your rental car experience.</p></div>
 
 </div>
 <div className='mid-art'><img className='art3' src={art3} alt="art3" />
-<div className='mid-art-abt'><p>May 7, 2023</p>
-<h4>Car Rental hacks for smooth Journey</h4>
-<p>Learn insider tips for getting the best deals and making the most of your rental car experience.</p></div></div>
+<div className='mid-art-abt'><p className="date">May 7, 2023</p>
+<h4 className="topic">Car Rental hacks for smooth Journey</h4>
+<p className="note">Learn insider tips for getting the best deals and making the most of your rental car experience.</p></div></div>
 <div className='mid-art'><img className='art4' src={art4} alt="art4" />
-<div className='mid-art-abt'><p>May 7, 2023</p>
-<h4>Car Rental hacks for smooth Journey</h4>
-<p>Learn insider tips for getting the best deals and making the most of your rental car experience.</p></div></div>
+<div className='mid-art-abt'><p className="date">May 7, 2023</p>
+<h4 className="topic">Car Rental hacks for smooth Journey</h4>
+<p className="note">Learn insider tips for getting the best deals and making the most of your rental car experience.</p></div></div>
 <div className='mid-art'><img className='art5' src={art5} alt="art5" />
-<div className='mid-art-abt'><p>May 7, 2023</p>
-<h4>Car Rental hacks for smooth Journey</h4>
-<p>Learn insider tips for getting the best deals and making the most of your rental car experience.</p></div></div>
+<div className='mid-art-abt'><p className="date">May 7, 2023</p>
+<h4 className="topic">Car Rental hacks for smooth Journey</h4>
+<p className="note">Learn insider tips for getting the best deals and making the most of your rental car experience.</p></div></div>
    
 </div>
 </div>
